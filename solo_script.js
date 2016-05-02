05451a781731cc520ea635bae5c2bfe81fe68b8c
Zach Kusz <zskusz@gmail.com>
2016-05-02 14:07:42 -0500

    initial commit

---
+++ solo_script.js
@@ -0,0 +1,85 @@
+// ! ! !
+// Three Bugs
+
+var arrayAtticus = ["Atticus", "2405", "47000", 3];
+var arrayJem = ["Jem", "62347", "63500", 4];
+var arrayBoo = ["Boo", "11435", "54000", 3];
+var arrayScout = ["Scout", "6243", "74750", 5];
+
+var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+
+//Create variables used to write to the DOM
+var newEl, newText;  //removed position and initialized it as a var on line 14
+//Capture the position of insertion into the DOM
+var position = document.getElementById('content');
+
+//Loop the array, extracting each array and writing information to the DOM
+//Note that the information is not 'clean'
+for(var i = 0; i < array.length; i++){
+	array[i] = calculateSTI(array[i]);
+ 	newEl = document.createElement('li');
+	newText = document.createTextNode(array[i]);
+	newEl.appendChild(newText);
+	position.appendChild(newEl);
+}
+
+function calculateSTI(array){
+  var newArray = [];
+
+  newArray[0] = array[0];
+
+  var employeeNumber = array[1];
+  var baseSalary = Number(array[2]); //added Number to convert from string
+  var reviewScore = array[3];
+
+  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  if(bonus > 0.13){
+    bonus = 0.13;
+
+  }
+
+  newArray[1] = bonus;
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus)); //added rounding
+  newArray[3] = Math.round(baseSalary * bonus);
+  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+  return newArray;
+}
+
+function getBaseSTI(reviewScore){
+  var basePercent;
+  switch(reviewScore){
+    case 1:
+      basePercent = 0;
+      break;
+    case 2:
+      basePercent = 0;
+      break;
+    case 3:
+      basePercent = 0.04;
+      break;
+    case 4:
+      basePercent = 0.06;
+      break;
+    case 5:
+      basePercent = 0.10;
+      break;
+  }
+  return basePercent; //why is it -1 ???? removed -1
+}
+
+function getYearAdjustment(employeeNumber){
+  var yearAdjustment = 0;
+  if(employeeNumber.length == 4){
+    yearAdjustment = 0.05;
+  }
+  return yearAdjustment;
+}
+
+function getIncomeAdjustment(salary){
+  var incomeAdjustment = 0;
+  salary = parseInt(salary);
+  if(salary > 65000){
+    incomeAdjustment = 0.01;
+  }
+  return incomeAdjustment;
+}
